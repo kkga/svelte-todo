ecb892c464d1b68dc1c55d362beae2a6eb041b4c
Gadzhi Kharkharov <me@kkga.me>
2021-03-07 23:18:59 +0300

    upd

--- src/App.svelte
+++ src/App.svelte
@@ -1,11 +1,11 @@
 <script>
-    import Todos from "./components/Todos.svelte";
+  import Todos from "./components/Todos.svelte";
 
-    let todos = [
-        { id: 1, name: "woot", completed: true },
-        { id: 2, name: "woot woot", completed: true },
-        { id: 3, name: "hey", completed: false },
-    ];
+  let todos = [
+    { id: 1, name: "woot", completed: true },
+    { id: 2, name: "woot woot", completed: true },
+    { id: 3, name: "hey", completed: false },
+  ];
 </script>
 
 <Todos {todos} />
